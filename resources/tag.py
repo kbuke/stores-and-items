404cc96243ac030ec83847a4114df2d2716688f5
Kaan Buke <kabuke13@gmail.com>
2025-08-13 12:42:18 +0200

    set up a many to many relationship between tags and items

--- resources/tag.py
+++ resources/tag.py
@@ -1,6 +1,6 @@
 from db import db 
-from models import StoreModel, TagModel 
-from schemas import TagSchema
+from models import StoreModel, TagModel, ItemModel 
+from schemas import TagSchema, TagAndItemSchema
 
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
@@ -29,3 +29,72 @@ class TagsInStore(MethodView):
         except SQLAlchemyError as e:
             abort(500, message=str(e))
         return tag 
+
+@blp.route("/tag")
+class TagList(MethodView):
+    @blp.response(200, TagSchema(many=True))
+    def get(self):
+        return TagModel.query.all()
+
+@blp.route("/item/<string:item_id>/tag/<string:tag_id>")
+class LinkTagsToItem(MethodView):
+    @blp.response(201, TagSchema)
+    def post(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        item.tags.append(tag)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occured while inserting the tag")
+        return tag
+
+    @blp.response(200, TagAndItemSchema)
+    def delete(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        item.tags.remove(tag)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occured while inserting the tag.")
+        return {
+            "message": "Item removed from tag", 
+            "item": item, 
+            "tag": tag
+        }
+
+@blp.route("/tag/<string:tag_id>")
+class Tag(MethodView):
+    @blp.response(200, TagSchema)
+    def get(self, tag_id):
+        tag = TagModel.query.get_or_404(tag_id)
+        return tag 
+
+    @blp.response(
+        202, 
+        description="Delete a tag if no item is tagged",
+        example={"message": "Tag deleted"}
+    )
+    @blp.alt_response(
+        404,
+        description="Tag not found"
+    )
+    def delete(self, tag_id):
+        tag = TagModel.query.get_or_404(tag_id)
+
+        if not tag.items:
+            db.session.delete(tag)
+            db.session.commit()
+            return{"message": "Tag deleted"}
+        abort(
+            400,
+            message="Could not delete tag. Make sure it is not attached to an item and try again"
+        )
+    
--- schemas.py
+++ schemas.py
@@ -1,7 +1,7 @@
 from marshmallow import Schema, fields 
 
 class PlainItemSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
 
@@ -11,12 +11,28 @@ class ItemUpdateSchema(Schema):
     store_id = fields.Int()
 
 class PlainStoreSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
 
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+
+class PlainTagSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str()
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
